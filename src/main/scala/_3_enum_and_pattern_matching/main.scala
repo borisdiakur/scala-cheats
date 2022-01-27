54dd43001edc102da8b96f3a459a223aeabcac27
borisdiakur <borisdiakur@gmail.com>
2022-01-27 22:08:48 +0100

    🐨

---
+++ src/main/scala/_3_enum_and_pattern_matching/main.scala
@@ -0,0 +1,15 @@
+package _3_enum_and_pattern_matching
+
+@main def main(): Unit = {
+  enum Sex {
+    case Male, Female
+  }
+
+  val mySex =
+    if ((new scala.util.Random).nextBoolean()) Sex.Male else Sex.Female
+
+  mySex match {
+    case Sex.Male   => println("I'm man! Make fire!")
+    case Sex.Female => println("I'm woman! Make kids!")
+  }
+}
