07bce0dcac73a91a433a244d3d20739814ad256e
borisdiakur <borisdiakur@gmail.com>
2022-02-15 21:51:58 +0100

    🦆

--- src/main/scala/_4_method/Operation.scala
+++ src/main/scala/_4_method/Operation.scala
@@ -2,9 +2,11 @@ package _4_method
 
 class Operation(var totalNumberOfThings: Int) {
   def +(summand: Int): Int = totalNumberOfThings + summand
+  def unary_- : Int = -totalNumberOfThings
 }
 
 @main def runOperation(): Unit = {
   val myOperation = new Operation(7)
   println(s"${myOperation + 3}")
+  println(s"${-myOperation}")
 }
